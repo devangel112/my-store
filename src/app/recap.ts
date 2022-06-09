53228e93bb4065d052ea4ef989deb38a381b3a3a
Angel Perez <dev.angel.perez@gmail.com>
2022-06-08 22:15:02 -0500

    Project Upload

---
+++ src/app/recap.ts
@@ -0,0 +1,14 @@
+const username: string = 'angel112';
+
+const sum = (num1: number, num2: number) => {
+  return num1 + num2;
+}
+
+sum(5, 7);
+
+
+class Person {
+  constructor(public age: number, public lastName: string) {
+
+  }
+}
